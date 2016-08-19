4249ecc23afe216272c568d6927ea78330b683dd
Infactum <infactum@gmail.com>
2016-08-19 16:14:42 +0500

    - pg init script optmization - fix #1 - line endings fix for .sh scripts

--- postgres-1c/01-setup_powa-archivist.sh
+++ postgres-1c/01-setup_powa-archivist.sh
@@ -2,10 +2,22 @@
 set -e
 
 # Configure shared_preload_libraries
-gosu postgres echo "shared_preload_libraries = 'pg_stat_statements,powa,pg_qualstats,pg_stat_kcache'" >> $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^shared_preload_libraries = '\(.*\)'.*$/shared_preload_libraries = '\1, pg_stat_statements, powa,pg_qualstats, pg_stat_kcache'/" $PGDATA/postgresql.conf
 
 # restart pg
 gosu postgres pg_ctl -D "$PGDATA" -w stop -m fast
 gosu postgres pg_ctl -D "$PGDATA" -w start
 
-gosu postgres psql -f /usr/local/src/enable_powa.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
+    CREATE EXTENSION hypopg;
+    CREATE database powa;
+    \c powa
+    CREATE EXTENSION pg_stat_statements;
+    CREATE EXTENSION btree_gist;
+    CREATE EXTENSION pg_qualstats;
+    CREATE EXTENSION pg_stat_kcache;
+    CREATE EXTENSION pg_track_settings;
+    CREATE EXTENSION powa;
+    \c template1
+    CREATE EXTENSION hypopg;
+EOSQL
--- postgres-1c/02-tune.sh
+++ postgres-1c/02-tune.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
-cp /usr/local/src/postgresql.conf $PGDATA/postgresql.conf
-chown postgres $PGDATA/postgresql.conf
+# perfomance tweaks based on PgTune online
+gosu postgres sed -i -e"s/^#\?max_connections =.*$/max_connections = 10/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?shared_buffers =.*$/shared_buffers = 512MB/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?effective_cache_size =.*$/effective_cache_size = 1536MB/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?work_mem =.*$/work_mem = 26214kB/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?maintenance_work_mem =.*$/maintenance_work_mem = 256MB/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?checkpoint_segments =.*$/checkpoint_segments = 128/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?checkpoint_completion_target =.*$/checkpoint_completion_target = 0.9/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?wal_buffers =.*$/wal_buffers = 16MB/" $PGDATA/postgresql.conf
+gosu postgres sed -i -e"s/^#\?default_statistics_target =.*$/default_statistics_target = 500/" $PGDATA/postgresql.conf
 
 # restart pg
 gosu postgres pg_ctl -D "$PGDATA" -w stop -m fast
